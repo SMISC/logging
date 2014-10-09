6120370c64973a13ab1a9fe2b1ab33d85c0e1f7c
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2014-10-09 05:22:31 -0700

    Make dummy scraper script

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -1,5 +1,9 @@
 from ..common.filtr import FilterJob
 from ..common.filtr import FilterService
-
 from ..common.scraper import ScrapeJob
 from ..common.scraper import ScrapeService
+
+import time
+
+while True:
+    time.sleep(1)
