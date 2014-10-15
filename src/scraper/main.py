431e69a4b0621b71a8adc2849f23a9b05d311337
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2014-10-15 15:02:30 -0700

    Remove singal handlers?

--- src/scraper/main.py
+++ src/scraper/main.py
@@ -70,8 +70,8 @@ class ScraperMain:
         print('[scraper-main] Polling for tweets for user ids starting at %d' % (last_tweet_id))
         self.stdout.flush()
         
-        signal.signal(signal.SIGINT, self.cleanup)
-        signal.signal(signal.SIGTERM, self.cleanup)
+        #signal.signal(signal.SIGINT, self.cleanup)
+        #signal.signal(signal.SIGTERM, self.cleanup)
 
         while True:
             recent_tweets = tweetservice.tweets_where('tweet_id > %s', [last_tweet_id])
