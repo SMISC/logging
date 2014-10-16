e5778cab08a0594084f3d4d2c34f6de8fa4943cc
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2014-10-16 05:19:08 -0700

    Wrap threads in logging

--- src/common/scraper.py
+++ src/common/scraper.py
@@ -13,28 +13,31 @@ class ScrapeFollowersJob(threading.Thread):
         self.edgeservice = edgeservice
         self.evt = evt
     def run(self):
-        while not self.evt.is_set():
-            self.evt.wait(5)
-            user_id = self.scrapeservice.dequeue('follow')
-            if user_id:
-                logging.debug('Scraping followers for %d', user_id)
-                cursor = -1
-                follower_ids = []
-                while not self.evt.is_set():
-                    resp = self.rlapi.request('followers/ids', {'user_id': user_id, 'count': 5000, 'cursor': cursor})
-                    for follower in resp.get_iterator():
-                        resp_follower_ids = []
+        try:
+            while not self.evt.is_set():
+                self.evt.wait(5)
+                user_id = self.scrapeservice.dequeue('follow')
+                if user_id:
+                    logging.debug('Scraping followers for %d', user_id)
+                    cursor = -1
+                    follower_ids = []
+                    while not self.evt.is_set():
+                        resp = self.rlapi.request('followers/ids', {'user_id': user_id, 'count': 5000, 'cursor': cursor})
+                        for follower in resp.get_iterator():
+                            resp_follower_ids = []
 
-                        if 'ids' in follower:
-                            for follower_id in follower['ids']:
-                                resp_follower_ids.append(str(follower_id))
-                                follower_ids.append(str(follower_id))
+                            if 'ids' in follower:
+                                for follower_id in follower['ids']:
+                                    resp_follower_ids.append(str(follower_id))
+                                    follower_ids.append(str(follower_id))
 
-                            self.edgeservice.add_follower_edges(user_id, resp_follower_ids)
-                            cursor = follower['next_cursor']
+                                self.edgeservice.add_follower_edges(user_id, resp_follower_ids)
+                                cursor = follower['next_cursor']
 
-                    if cursor <= 0:
-                        break
+                        if cursor <= 0:
+                            break
+        except Exception as err:
+            logging.exception('Caught error: %s' % (str(err)))
 
 class ScrapeInfoJob(threading.Thread):
     def __init__(self, rlapi, userservice, scrapeservice, evt):
@@ -44,30 +47,33 @@ class ScrapeInfoJob(threading.Thread):
         self.userservice = userservice
         self.evt = evt
     def run(self):
-        while not self.evt.is_set():
-            ids = []
-            t = 15 # wait up to 15 seconds, otherwise we're behind on average
-            while not self.evt.is_set() and len(ids) < 100 and t > 0:
-                user_id = self.scrapeservice.dequeue('info')
-                if user_id:
-                    ids.append(int(user_id))
-                t = t - 1
-                self.evt.wait(1)
-            logging.debug('Scraping info for %d users', len(ids))
+        try:
+            while not self.evt.is_set():
+                ids = []
+                t = 15 # wait up to 15 seconds, otherwise we're behind on average
+                while not self.evt.is_set() and len(ids) < 100 and t > 0:
+                    user_id = self.scrapeservice.dequeue('info')
+                    if user_id:
+                        ids.append(int(user_id))
+                    t = t - 1
+                    self.evt.wait(1)
+                logging.debug('Scraping info for %d users', len(ids))
 
-            if len(ids) > 0:
-                resp = self.rlapi.request('users/lookup', {'user_id': ','.join(ids)})
-                for user in resp.get_iterator():
-                    self.userservice.create_user({
-                        'user_id': user['id'],
-                        'screen_name': user['screen_name'],
-                        'total_tweets': user['statuses_count'],
-                        'followers': user['followers_count'],
-                        'following': user['friends_count'],
-                        'full_name': user['name'],
-                        'bio': user['description'],
-                        'timestamp': twittertime(user['created_at'])
-                    })
+                if len(ids) > 0:
+                    resp = self.rlapi.request('users/lookup', {'user_id': ','.join(ids)})
+                    for user in resp.get_iterator():
+                        self.userservice.create_user({
+                            'user_id': user['id'],
+                            'screen_name': user['screen_name'],
+                            'total_tweets': user['statuses_count'],
+                            'followers': user['followers_count'],
+                            'following': user['friends_count'],
+                            'full_name': user['name'],
+                            'bio': user['description'],
+                            'timestamp': twittertime(user['created_at'])
+                        })
+        except Exception as err:
+            logging.exception('Caught error: %s' % (str(err)))
 
 class ScrapeService:
     def __init__(self, rds, scan_id):
