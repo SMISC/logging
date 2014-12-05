39313258b8d4a8c6427e80710079ac5f241afa8d
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2014-12-05 12:03:47 +0000

    Login to be centered

--- provision/dashboard/app/views/login.blade.php
+++ provision/dashboard/app/views/login.blade.php
@@ -1,17 +1,28 @@
 @extends('base')
 
+@section('head')
+@parent
+<link rel="stylesheet" href="/css/login.css" />
+@stop
+
 @section('content')
 
 @if ($message)
 <p> {{{ $message }}} </p>
 @endif
 
-<form method="post" action="/login">
-    <label>
-        Password:
-        <input type="password" name="password" />
-    </label>
+<table id="login">
+    <tr>
+        <td>
+            <form method="post" action="/login">
+                <label>
+                    Password:
+                    <input type="password" name="password" />
+                </label>
 
-    <input type="submit" value="Login" />
-</form>
+                <input type="submit" value="Login" />
+            </form>
+        </td>
+    </tr>
+</table>
 @stop
