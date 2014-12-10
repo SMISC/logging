195cab1c8108d3c8198d9833b875205b5be496dd
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2014-12-10 03:17:55 +0000

    Add more clients

--- provision/schema/schema.sql
+++ provision/schema/schema.sql
@@ -21,7 +21,9 @@ SET default_with_oids = false;
 CREATE TABLE backups (
     id integer NOT NULL,
     scan_id integer,
-    "timestamp" integer
+    "timestamp" integer,
+    ref_count bigint,
+    glacier_id text
 );
 
 
@@ -32,11 +34,11 @@ ALTER TABLE public.backups OWNER TO pacsocial;
 --
 
 CREATE SEQUENCE backups_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
 
 
 ALTER TABLE public.backups_id_seq OWNER TO pacsocial;
@@ -49,12 +51,47 @@ ALTER SEQUENCE backups_id_seq OWNED BY backups.id;
 
 
 --
+-- Name: point; Type: TABLE; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+CREATE TABLE point (
+    id integer NOT NULL,
+    type character(15),
+    entity_ref bigint,
+    bot_id character varying(32)
+);
+
+
+ALTER TABLE public.point OWNER TO pacsocial;
+
+--
+-- Name: point_id_seq; Type: SEQUENCE; Schema: public; Owner: pacsocial
+--
+
+CREATE SEQUENCE point_id_seq
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
+
+
+ALTER TABLE public.point_id_seq OWNER TO pacsocial;
+
+--
+-- Name: point_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: pacsocial
+--
+
+ALTER SEQUENCE point_id_seq OWNED BY point.id;
+
+
+--
 -- Name: scan; Type: TABLE; Schema: public; Owner: pacsocial; Tablespace: 
 --
 
 CREATE TABLE scan (
     id integer NOT NULL,
-    type character varying(10),
+    type character varying(50),
     start integer,
     "end" integer,
     ref_start character(32),
@@ -83,11 +120,11 @@ COMMENT ON COLUMN scan.ref_end IS 'inclusive';
 --
 
 CREATE SEQUENCE scan_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
 
 
 ALTER TABLE public.scan_id_seq OWNER TO pacsocial;
@@ -98,6 +135,83 @@ ALTER TABLE public.scan_id_seq OWNER TO pacsocial;
 
 ALTER SEQUENCE scan_id_seq OWNED BY scan.id;
 
+
+--
+-- Name: targets; Type: TABLE; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+CREATE TABLE targets (
+    twitter_id character varying(32)
+);
+
+
+ALTER TABLE public.targets OWNER TO pacsocial;
+
+--
+-- Name: team; Type: TABLE; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+CREATE TABLE team (
+    id integer NOT NULL,
+    name character varying(32)
+);
+
+
+ALTER TABLE public.team OWNER TO pacsocial;
+
+--
+-- Name: team_bot; Type: TABLE; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+CREATE TABLE team_bot (
+    team_id integer,
+    twitter_id character varying(32),
+    screen_name character varying(32),
+    type integer DEFAULT 0
+);
+
+
+ALTER TABLE public.team_bot OWNER TO pacsocial;
+
+--
+-- Name: COLUMN team_bot.type; Type: COMMENT; Schema: public; Owner: pacsocial
+--
+
+COMMENT ON COLUMN team_bot.type IS '0 = scorer, 1 = helper';
+
+
+--
+-- Name: team_id_seq; Type: SEQUENCE; Schema: public; Owner: pacsocial
+--
+
+CREATE SEQUENCE team_id_seq
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
+
+
+ALTER TABLE public.team_id_seq OWNER TO pacsocial;
+
+--
+-- Name: team_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: pacsocial
+--
+
+ALTER SEQUENCE team_id_seq OWNED BY team.id;
+
+
+--
+-- Name: team_link; Type: TABLE; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+CREATE TABLE team_link (
+    team_id integer,
+    link text
+);
+
+
+ALTER TABLE public.team_link OWNER TO pacsocial;
 
 --
 -- Name: tuser; Type: TABLE; Schema: public; Owner: pacsocial; Tablespace: 
@@ -129,11 +243,11 @@ ALTER TABLE public.tuser OWNER TO pacsocial;
 --
 
 CREATE SEQUENCE tuser_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
 
 
 ALTER TABLE public.tuser_id_seq OWNER TO pacsocial;
@@ -154,22 +268,58 @@ CREATE TABLE tuser_tuser (
     from_user character varying(32),
     to_user character varying(32),
     weight smallint,
-    id integer NOT NULL
+    id integer NOT NULL,
+    bot boolean
 );
 
 
 ALTER TABLE public.tuser_tuser OWNER TO pacsocial;
 
 --
+-- Name: tuser_tuser_bot; Type: TABLE; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+CREATE TABLE tuser_tuser_bot (
+    id integer NOT NULL,
+    from_user character varying(32),
+    to_user character varying(32),
+    "timestamp" integer
+);
+
+
+ALTER TABLE public.tuser_tuser_bot OWNER TO pacsocial;
+
+--
+-- Name: tuser_tuser_bot_id_seq; Type: SEQUENCE; Schema: public; Owner: pacsocial
+--
+
+CREATE SEQUENCE tuser_tuser_bot_id_seq
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
+
+
+ALTER TABLE public.tuser_tuser_bot_id_seq OWNER TO pacsocial;
+
+--
+-- Name: tuser_tuser_bot_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: pacsocial
+--
+
+ALTER SEQUENCE tuser_tuser_bot_id_seq OWNED BY tuser_tuser_bot.id;
+
+
+--
 -- Name: tuser_tuser_id_seq; Type: SEQUENCE; Schema: public; Owner: pacsocial
 --
 
 CREATE SEQUENCE tuser_tuser_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
 
 
 ALTER TABLE public.tuser_tuser_id_seq OWNER TO pacsocial;
@@ -204,7 +354,7 @@ ALTER TABLE public.tweet OWNER TO pacsocial;
 CREATE TABLE tweet_entity (
     tweet_id bigint,
     type character varying(20),
-    text character varying(140),
+    text text,
     id integer NOT NULL
 );
 
@@ -216,11 +366,11 @@ ALTER TABLE public.tweet_entity OWNER TO pacsocial;
 --
 
 CREATE SEQUENCE tweet_entity_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
 
 
 ALTER TABLE public.tweet_entity_id_seq OWNER TO pacsocial;
@@ -237,11 +387,11 @@ ALTER SEQUENCE tweet_entity_id_seq OWNED BY tweet_entity.id;
 --
 
 CREATE SEQUENCE tweet_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+START WITH 1
+INCREMENT BY 1
+NO MINVALUE
+NO MAXVALUE
+CACHE 1;
 
 
 ALTER TABLE public.tweet_id_seq OWNER TO pacsocial;
@@ -264,7 +414,21 @@ ALTER TABLE ONLY backups ALTER COLUMN id SET DEFAULT nextval('backups_id_seq'::r
 -- Name: id; Type: DEFAULT; Schema: public; Owner: pacsocial
 --
 
+ALTER TABLE ONLY point ALTER COLUMN id SET DEFAULT nextval('point_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: pacsocial
+--
+
 ALTER TABLE ONLY scan ALTER COLUMN id SET DEFAULT nextval('scan_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: pacsocial
+--
+
+ALTER TABLE ONLY team ALTER COLUMN id SET DEFAULT nextval('team_id_seq'::regclass);
 
 
 --
@@ -279,6 +443,13 @@ ALTER TABLE ONLY tuser ALTER COLUMN id SET DEFAULT nextval('tuser_id_seq'::regcl
 --
 
 ALTER TABLE ONLY tuser_tuser ALTER COLUMN id SET DEFAULT nextval('tuser_tuser_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: pacsocial
+--
+
+ALTER TABLE ONLY tuser_tuser_bot ALTER COLUMN id SET DEFAULT nextval('tuser_tuser_bot_id_seq'::regclass);
 
 
 --
@@ -300,7 +471,15 @@ ALTER TABLE ONLY tweet_entity ALTER COLUMN id SET DEFAULT nextval('tweet_entity_
 --
 
 ALTER TABLE ONLY scan
-    ADD CONSTRAINT scan_pkey PRIMARY KEY (id);
+ADD CONSTRAINT scan_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: team_pkey; Type: CONSTRAINT; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+ALTER TABLE ONLY team
+ADD CONSTRAINT team_pkey PRIMARY KEY (id);
 
 
 --
@@ -308,6 +487,13 @@ ALTER TABLE ONLY scan
 --
 
 CREATE INDEX "by id" ON tuser USING btree (user_id);
+
+
+--
+-- Name: by_tuser_id; Type: INDEX; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+CREATE INDEX by_tuser_id ON team_bot USING btree (twitter_id);
 
 
 --
@@ -332,6 +518,27 @@ CREATE INDEX bytweet ON tweet_entity USING btree (tweet_id);
 
 
 --
+-- Name: targets_by_id; Type: INDEX; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+CREATE INDEX targets_by_id ON targets USING btree (twitter_id);
+
+
+--
+-- Name: tuser_tuser_bot_touser; Type: INDEX; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+CREATE INDEX tuser_tuser_bot_touser ON tuser_tuser_bot USING btree (to_user, from_user);
+
+
+--
+-- Name: tweet_by_tweetid; Type: INDEX; Schema: public; Owner: pacsocial; Tablespace: 
+--
+
+CREATE INDEX tweet_by_tweetid ON tweet USING btree (tweet_id);
+
+
+--
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
 
@@ -352,6 +559,16 @@ GRANT SELECT ON TABLE backups TO dashboard;
 
 
 --
+-- Name: point; Type: ACL; Schema: public; Owner: pacsocial
+--
+
+REVOKE ALL ON TABLE point FROM PUBLIC;
+REVOKE ALL ON TABLE point FROM pacsocial;
+GRANT ALL ON TABLE point TO pacsocial;
+GRANT SELECT ON TABLE point TO dashboard;
+
+
+--
 -- Name: scan; Type: ACL; Schema: public; Owner: pacsocial
 --
 
@@ -359,6 +576,46 @@ REVOKE ALL ON TABLE scan FROM PUBLIC;
 REVOKE ALL ON TABLE scan FROM pacsocial;
 GRANT ALL ON TABLE scan TO pacsocial;
 GRANT SELECT ON TABLE scan TO dashboard;
+
+
+--
+-- Name: targets; Type: ACL; Schema: public; Owner: pacsocial
+--
+
+REVOKE ALL ON TABLE targets FROM PUBLIC;
+REVOKE ALL ON TABLE targets FROM pacsocial;
+GRANT ALL ON TABLE targets TO pacsocial;
+GRANT SELECT ON TABLE targets TO dashboard;
+
+
+--
+-- Name: team; Type: ACL; Schema: public; Owner: pacsocial
+--
+
+REVOKE ALL ON TABLE team FROM PUBLIC;
+REVOKE ALL ON TABLE team FROM pacsocial;
+GRANT ALL ON TABLE team TO pacsocial;
+GRANT SELECT ON TABLE team TO dashboard;
+
+
+--
+-- Name: team_bot; Type: ACL; Schema: public; Owner: pacsocial
+--
+
+REVOKE ALL ON TABLE team_bot FROM PUBLIC;
+REVOKE ALL ON TABLE team_bot FROM pacsocial;
+GRANT ALL ON TABLE team_bot TO pacsocial;
+GRANT SELECT ON TABLE team_bot TO dashboard;
+
+
+--
+-- Name: team_link; Type: ACL; Schema: public; Owner: pacsocial
+--
+
+REVOKE ALL ON TABLE team_link FROM PUBLIC;
+REVOKE ALL ON TABLE team_link FROM pacsocial;
+GRANT ALL ON TABLE team_link TO pacsocial;
+GRANT SELECT ON TABLE team_link TO dashboard;
 
 
 --
@@ -379,6 +636,16 @@ REVOKE ALL ON TABLE tuser_tuser FROM PUBLIC;
 REVOKE ALL ON TABLE tuser_tuser FROM pacsocial;
 GRANT ALL ON TABLE tuser_tuser TO pacsocial;
 GRANT SELECT ON TABLE tuser_tuser TO dashboard;
+
+
+--
+-- Name: tuser_tuser_bot; Type: ACL; Schema: public; Owner: pacsocial
+--
+
+REVOKE ALL ON TABLE tuser_tuser_bot FROM PUBLIC;
+REVOKE ALL ON TABLE tuser_tuser_bot FROM pacsocial;
+GRANT ALL ON TABLE tuser_tuser_bot TO pacsocial;
+GRANT SELECT ON TABLE tuser_tuser_bot TO dashboard;
 
 
 --
