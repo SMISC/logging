8fd9ae459bc805f0addf334ae19e81531940bb98
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2014-12-15 08:44:28 +0000

    Add a key to block new queues instead of purging cron

--- provision/src/service/lock.py
+++ provision/src/service/lock.py
@@ -6,35 +6,41 @@ logger = logging.getLogger(__name__)
 class LockService:
     def __init__(self, rds, key):
         self.rds = rds
-        self.key = 'lock_%s' % (key)
+        self.lock_key = 'lock_%s' % (key)
+        self.block_key = 'block_%s' % (key)
         self.acquired = None
 
     def acquire(self, expire_time = 3600):
-        rv = self.rds.setnx(self.key, time.time())
-        self.acquired = rv
+        blocked = self.rds.get(self.block_key)
+        rv = self.rds.setnx(self.lock_key, time.time())
 
-        if not rv:
+        if blocked:
+            self.acquired = False
+            logger.info('%s manually blocked from running', self.lock_key)
+        elif not rv:
+            self.acquired = False
             time_started = self.inspect()
-            logger.info('%s locked (since %.0f minutes ago).', self.key, (time.time() - time_started) / 60)
+            logger.info('%s locked (since %.0f minutes ago).', self.lock_key, (time.time() - time_started) / 60)
         else:
+            self.acquired = True
             if expire_time is not None:
-                self.rds.expire(self.key, expire_time)
-            logger.debug('%s acquired and now locked', self.key)
+                self.rds.expire(self.lock_key, expire_time)
+            logger.debug('%s acquired and now locked', self.lock_key)
 
-        return rv
+        return self.acquired
 
     def get_did_acquire(self):
         return self.acquired
 
     def inspect(self):
-        val = self.rds.get(self.key)
+        val = self.rds.get(self.lock_key)
         if val:
             return float(val)
         return None
 
     def release(self):
         if self.acquired:
-            logger.debug('%s released', self.key)
-            self.rds.delete(self.key)
+            logger.debug('%s released', self.lock_key)
+            self.rds.delete(self.lock_key)
         else:
             logger.exception('tried to free a lock what was never locked. silently ignoring....')
