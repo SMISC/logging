0436b1310904ac77e3f6f4c0fd10529c3ec6ea7f
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2015-01-22 03:38:07 +0000

    Add 5-node cluster commands; add cluster admin commands

--- provision/hosts.py
+++ provision/hosts.py
@@ -1,13 +1,22 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 
 import sys
 import json
+import tempfile
 
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        fdt = tempfile.NamedTemporaryFile(delete=False)
+    else:
+        fdt = sys.stdout
+
     for line in sys.stdin:
         droplets = json.loads(line)
         for droplet in droplets['droplets']:
             if droplet['status'] == 'active':
                 host = droplet['name']
                 ip = droplet['ip_address']
-                print('%s %s.jacobgreenleaf.com %s' % (ip, host, host))
+                fdt.write("%s %s.jacobgreenleaf.com %s\n" % (ip, host, host))
+    
+    if len(sys.argv) > 1:
+        print(fdt.name)
